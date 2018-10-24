7576d49938267816fba06a3bad47c3c49bdc9409
J/A <archer884@gmail.com>
2018-10-24 00:41:11 -0500

    Add fixme notes

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,13 @@ mod sync;
 
 use std::io;
 
+// FIXME:
+//
+// - This stupid tool is way too happy to copy over things like .DS_Store files.
+// - Add a dry run mode. (Duh?! How did you get the real thing without a trial version?)
+// - Implement two-way mode.
+// - Add 'silent' mode.
+
 fn main() -> io::Result<()> {
     options::Opt::from_args().execute()
 }
